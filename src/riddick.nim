a8e042e0f90aaa571670231cdfc95a3cfae5ab11
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2022-10-05 17:16:30 -0500

    First function added to the project

--- src/riddick.nim
+++ src/riddick.nim
@@ -1,4 +1,16 @@
-echo "Hello"
-echo "What's your name? "
-var name: string = readLine(stdin)
-echo "Hi, ", name, "!"
+import std/strutils
+
+echo "|====== Riddick ======|"
+
+let path = "examples/dictionary.txt"
+
+proc addYears(dictionaryPath: string) =
+    for line in lines dictionaryPath:
+        echo line.toUpper , "2002"
+        echo line , "2022"
+
+addYears(path)
+
+for line in lines path:
+    echo line.toUpper[0]
+    echo line
